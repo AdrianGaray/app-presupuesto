f60aa7b2f2445425364a011357402b713c0505b6
AdrianGaray <adriangaray396@gmail.com>
2026-02-24 20:36:55 -0300

    Capítulo 56 -  Separación Plantilla HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceroComponent } from "./cabecero/cabecero.component";
+import { FormularioComponent } from "./formulario/formulario.component";
+import { EgresoComponent } from "./egreso/egreso.component";
+import { IngresoComponent } from "./ingreso/ingreso.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceroComponent, FormularioComponent, EgresoComponent, IngresoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
